be26c3dceb23dc4006df395301dcd9f48bc4a33d
Changjun Yeo <lunaradio24@gmail.com>
2024-05-07 19:42:50 +0900

    fix: search.js에서 4개의 영화리스트 모두 검색 적용되게끔

--- src/main.js
+++ src/main.js
@@ -21,10 +21,20 @@ function addEvents(fetchedMovies, fetchedCredits) {
   // 정렬 버튼 이벤트
   // document.querySelector(selector) : CSS 선택자를 이용하여 요소를 선택합니다.
   //HTML 문서에서  id #sortTitleUp 요소 선택하여 sortButton1에 할당
-  const sortButton1 = document.querySelector("#sortTitleUp");
+  const sortButton1 = document.getElementById("sortTitleUp");
   // 버튼 1번에 이벤트리스턴널
   sortButton1.addEventListener("click", () => {
-    sortMovie();
+    //sessionStorage에 저장된 sortKey를 가져옴
+    let sortKey = sessionStorage.getItem("sortKey");
+    //sortKey가 true면 오름차순 false면 내림차순
+    sortMovie("now-playing", sortKey);
+    sortMovie("top-rated", sortKey);
+    sortMovie("popular", sortKey);
+    sortMovie("upcoming", sortKey);
+    // 논리 부정 연산자를 사용하여 sortKey를 반대로 저장
+    if (sortKey === "ascending") sessionStorage.setItem("sortKey", "descending");
+    else if (sortKey === "descending") sessionStorage.setItem("sortKey", "ascending");
+    else console.log("sortKey의 저장된 값에 오류가 있습니다.");
   });
 }
 
@@ -38,11 +48,23 @@ document.addEventListener(
     const topRated = await fetchMovies("top_rated");
     const popular = await fetchMovies("popular");
     const upcoming = await fetchMovies("upcoming");
+    //영화크레딧 불러와서 저장
+    const nowPlayingCredits = fetchCredits(nowPlaying);
+    const topRatedCredits = fetchCredits(topRated);
+    const popularCredits = fetchCredits(popular);
+    const upcomingCredits = fetchCredits(upcoming);
+
+    //영화데이터, 영화크레딧 통합
+    const fetchedMovies = [...nowPlaying, ...topRated, ...popular, ...upcoming];
+    const fetchedCredits = [nowPlayingCredits, topRatedCredits, popularCredits, upcomingCredits];
+
     //영화카드 생성
     makeCard(nowPlaying, "now-playing"); // fetch된 영화의 카드를 생성할 리스트를 id로 선택
     makeCard(topRated, "top-rated");
     makeCard(popular, "popular");
     makeCard(upcoming, "upcoming");
+    //sessionStorage에 저장된 sortKey를 "ascending"로 초기화
+    sessionStorage.setItem("sortKey", "ascending");
     //클릭 이벤트를 모두 생성
     addEvents(fetchedMovies, fetchedCredits);
   },
